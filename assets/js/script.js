ba7e84bacc6dacee807369128f749554b9b6beab
Eric Ritchey <eric@eandjdesign.com>
2014-12-11 16:26:28 -0800

    Adding mixins, helpers and image preloading, oh my!

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,11 @@ Features
 
 ------------------------------------------------------------ */
 
-// --- set up TSLA Object
-var TSLA = TSLA || {};
+// --- set up OBJ Object
+var OBJ = OBJ || {};
 
 // --- set up keys available
-TSLA.Keys = {
+OBJ.Keys = {
     UP: 38,
     DOWN: 40,
     LEFT: 37,
@@ -34,27 +34,10 @@ $(document).ready(function(){
 
     // --- set up variables
     var $theatre     = $('.image-theatre'),
+        $gallery     = $('.gallery-container'),
         theatreImage = $theatre.find('.theatre-image');
 
-    $('.image-theatre').on('click', '.theatre-close', function(e) {
-        drawCurtainsOnTheatre($(this));
-    });
-
-    $('.gallery-container').on('click', '.image-link', function(e) {
-        e.preventDefault();
-
-        // --- set up variables
-        var $this = $(this),
-            theatreImgSrc = $this.attr('href');
-
-        // --- set the initial theatre image
-        setTheatreImage(theatreImgSrc);
-
-        // --- let the show begin!
-        openCurtainsOnTheatre();
-    });
-
-    $('.theatre-thumbs').on('click', '.image-link', function(e) {
+    var handleImageClick = function(e){
         e.preventDefault();
 
         var $this = $(this),
@@ -65,33 +48,45 @@ $(document).ready(function(){
         switchActiveThumb($this.parent());
 
         setTheatreImage(theatreImgSrc);
-    });
+    };
 
-    var keyPressFunc = function(e) {
+    var handleKeyPress = function(e) {
 
         var activeThumb = $theatre.find('.thumb-active'),
             prevThumb   = activeThumb.prevWrap(),
             nextThumb   = activeThumb.nextWrap();
 
-        if(e.keyCode === TSLA.Keys.ESCAPE) {
+        if(e.keyCode === OBJ.Keys.ESCAPE) {
             drawCurtainsOnTheatre();
 
-        } else if(e.keyCode === TSLA.Keys.RIGHT || e.keyCode === TSLA.Keys.DOWN) {
-            nextThumb.find('.image-link').trigger('click');
+        } else if(e.keyCode === OBJ.Keys.RIGHT || e.keyCode === OBJ.Keys.DOWN) {
+            handleImageClick.call(nextThumb.find('.image-link'), e);
+            // nextThumb.find('.image-link').trigger('click');
 
-        } else if(e.keyCode === TSLA.Keys.LEFT || e.keyCode === TSLA.Keys.UP) {
-            prevThumb.find('.image-link').trigger('click');
+        } else if(e.keyCode === OBJ.Keys.LEFT || e.keyCode === OBJ.Keys.UP) {
+            handleImageClick.call(prevThumb.find('.image-link'), e);
+            // prevThumb.find('.image-link').trigger('click');
         }
-    }
+    };
 
     // Bind Keys to the Theatre -----------------------------------
     function bindTheatreKeys() {
-        $(document).on('keyup.gallery', keyPressFunc);
+        $(document).on('keyup.gallery', handleKeyPress);
     }
 
     // Unbind Keys to the Theatre ---------------------------------
     function unbindTheatreKeys() {
-        $(document).off('keyup.gallery', keyPressFunc);
+        $(document).off('keyup.gallery', handleKeyPress);
+    }
+
+    // Clone thumbnails to theatre --------------------------------
+    function setThumbnails(element) {
+        element.clone().appendTo($theatre.find('.thumbs-container'));
+    }
+
+    // Remove thumbnails from theatre -----------------------------
+    function removeThumbnails() {
+        $theatre.find('.thumbs-container .theatre-thumbs').remove();
     }
 
     // Swap Theatre image -----------------------------------------
@@ -102,6 +97,7 @@ $(document).ready(function(){
     // Close Theatre ----------------------------------------------
     function drawCurtainsOnTheatre() {
         $theatre.removeClass('open-curtains');
+        removeThumbnails();
         unbindTheatreKeys();
     }
 
@@ -115,17 +111,61 @@ $(document).ready(function(){
     function switchActiveThumb(thumb) {
         thumb.addClass('thumb-active').siblings('.thumb-active').removeClass('thumb-active');
     }
+
+
+    $('.image-theatre').on('click', '.theatre-close', function(e) {
+        drawCurtainsOnTheatre();
+    });
+
+    $gallery.on('click', '.image-link', function(e) {
+        e.preventDefault();
+
+        // --- set up variables
+        var $this = $(this),
+            theatreImgSrc = $this.attr('href'),
+            thumbnailElement = $this.closest('.image-container').siblings('.thumbs-container').children('.theatre-thumbs');
+
+        // --- clone the thumbnail markup into the theatre
+        setThumbnails(thumbnailElement);
+
+        // --- set the initial theatre image
+        setTheatreImage(theatreImgSrc);
+
+        // --- let the show begin!
+        openCurtainsOnTheatre();
+
+        // --- activate thumbnail clicking
+        $('.theatre-thumbs').on('click', '.image-link', handleImageClick);
+
+    });
+
+    // ------------------------------------------------------------
+    (function($) {
+        $.fn.nextWrap = function() {
+            var $next = this.next();
+            return ($next.length === 0) ? this.siblings().first() : $next;
+        };
+
+        $.fn.prevWrap = function() {
+            var $prev = this.prev();
+            return ($prev.length === 0) ? this.siblings().last() : $prev;
+        };
+
+        $.fn.preload = function() {
+            this.each(function(){
+                $('<img/>')[0].src = this;
+            });
+        }
+    })(jQuery);
+
 });
 
-// ------------------------------------------------------------
-(function($) {
-    $.fn.nextWrap = function() {
-        var $next = this.next();
-        return ($next.length === 0) ? this.siblings().first() : $next;
-    };
+window.onload = function() {
+    var images = $('.gallery-container').find('.image-link'),
+        preloadImagesArray = [];
 
-    $.fn.prevWrap = function() {
-        var $prev = this.prev();
-        return ($prev.length === 0) ? this.siblings().last() : $prev;
-    };
-})(jQuery);
+    $.each(images, function(key, value) {
+        preloadImagesArray.push($(images[key]).attr('href'));
+        $(preloadImagesArray).preload();
+    });
+}
